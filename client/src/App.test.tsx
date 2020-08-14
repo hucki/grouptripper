b773d8a0d8b482817c59fb26671d77fe5ea62077
Alex Marshall <alex.k.marshall83@gmail.com>
2020-08-14 17:21:04 +0100

    ci(linting) Set up precommit linting

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
 
-test("renders app header", () => {
+test('renders app header', () => {
   const { getByText } = render(<App />);
   expect(getByText(/group tripper/i)).toBeInTheDocument();
 });
