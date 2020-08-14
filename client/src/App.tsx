f5632a3fada8031461d7a87738416eee7df739a7
Alex Marshall <alex.k.marshall83@gmail.com>
2020-08-14 18:36:11 +0100

    refactor(nav): Extract Navigation to component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
 
-function App() {
+function App(): JSX.Element {
   return (
     <Router>
-      <header className="container flex flex-row justify-between m-6 mx-auto align-middle">
-        <h1 className="text-xl text-red-800">Group Tripper</h1>
-        <nav>
-          <ul className="flex flex-row space-x-6">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/create-trip">Create Trip</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <Switch>
-        <Route path="/create-trip">
-          <div>Create a trip</div>
-        </Route>
-        <Route path="/">
-          <div>Here's a list of trips</div>
-        </Route>
-      </Switch>
+      <div className="container mx-auto">
+        <Navigation />
+        <Switch>
+          <Route path="/create-trip">
+            <div>Create a trip</div>
+          </Route>
+          <Route path="/">
+            <div>Here's a list of trips</div>
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
