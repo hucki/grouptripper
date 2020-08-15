c48fcdef90e9cdbefa91806d0d2cc41859878ae2
Alex Marshall <alex.k.marshall83@gmail.com>
2020-08-15 09:51:24 +0100

    feat(api): Create client function to access api

--- client/src/services/ApiClient.tsx
+++ client/src/services/ApiClient.tsx
@@ -1,7 +1,7 @@
-const apiUrl =
+const routingApiUrl =
   process.env.REACT_APP_ROUTING_API_URL || 'https://api.openrouteservice.org/';
 // TODO: get API_KEY securly
-const apiKey = process.env.REACT_APP_ROUTING_API_KEY || '';
+const routingApiKey = process.env.REACT_APP_ROUTING_API_KEY || '';
 
 // request POIs
 const reqBody =
@@ -10,10 +10,10 @@ const endpoint = 'pois';
 
 const ApiClient = {
   getPois: (): Promise<any> => {
-    return fetch(`${apiUrl}${endpoint}`, {
+    return fetch(`${routingApiUrl}${endpoint}`, {
       headers: {
         'content-type': 'application/json',
-        Authorization: apiKey,
+        Authorization: routingApiKey,
       },
       body: reqBody,
     }).then((res) => res.json());
@@ -21,3 +21,36 @@ const ApiClient = {
 };
 
 export default ApiClient;
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+type clientOptions<T> = {
+  data?: T;
+};
+
+export function client<T>(
+  endpoint: string,
+  { data }: clientOptions<T>
+): Promise<T> {
+  const headers = new Headers();
+  if (data) headers.append('Content-Type', 'application/json');
+
+  const config = {
+    method: data ? 'POST' : 'GET',
+    body: data ? JSON.stringify(data) : undefined,
+    headers,
+  };
+
+  const request = new Request(`${apiUrl}/${endpoint}`, config);
+
+  return window
+    .fetch(`${apiUrl}/${endpoint}`, config)
+    .then(async (response) => {
+      const data = await response.json();
+      if (response.ok) {
+        return data;
+      } else {
+        return Promise.reject(data);
+      }
+    });
+}
