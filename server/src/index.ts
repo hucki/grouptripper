f8aad3cb042e4980d3d66150bcddd07bfb1bdbdc
Alex Marshall <alex.k.marshall83@gmail.com>
2020-08-17 09:27:26 +0100

    Refactor(server): Extract default error handling

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
@@ -13,6 +13,16 @@ app.use(express.json());
 app.use(morgan('tiny'));
 
 app.use('/', tripsRouter);
+
+// eslint-disable-next-line no-unused-vars
+app.use((err: Error, req: Request, res: Response) => {
+  console.error(err.message, err);
+  return res.status(500).json({
+    message: err.message,
+    // we only add a `stack` property in non-production environments
+    ...(process.env.NODE_ENV === 'production' ? null : { stack: err.stack }),
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
