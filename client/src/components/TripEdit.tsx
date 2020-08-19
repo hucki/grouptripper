241cf784ab24c5e5647973cafc7cc4500ec3786f
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-08-19 19:07:05 +0200

    feat(tripedit): add popout Editcard per stop

--- client/src/components/TripEdit.tsx
+++ client/src/components/TripEdit.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MapContainer from './MapContainer';
 import { Trip } from '../types/Trip';
 import { Stop } from '../types/Stop';
@@ -7,14 +7,17 @@ import { client } from '../services/ApiClient';
 import { useQuery } from 'react-query';
 import TripCard from './TripCard';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Formik, Form, Field } from 'formik';
 import {
   faChevronCircleUp,
   faChevronCircleDown,
   faSave,
   faEdit,
 } from '@fortawesome/free-solid-svg-icons';
+import { string } from 'yup';
 
 export default function TripEdit(): JSX.Element {
+  const [editStop, setEditStop] = useState('');
   const { id } = useParams();
   const { isLoading, error, data } = useQuery('trip', () =>
     client<Trip>(`trips/${id}`)
@@ -29,33 +32,64 @@ export default function TripEdit(): JSX.Element {
     stop: Stop;
   };
 
-  const TimelineItem = ({ stop }: StopCardPropTypes): JSX.Element => {
+  const StopListItem = ({ stop }: StopCardPropTypes): JSX.Element => {
     return (
-      <li className="flex flex-row mb-2 border-gray-400">
-        <div className="flex items-center flex-1 p-2 transition duration-500 ease-in-out transform bg-gray-200 rounded-md cursor-pointer select-none hover:-translate-y-1 hover:shadow-lg">
-          <div className="flex flex-col items-center justify-center w-10 h-10 mr-4 bg-gray-300 rounded-md">
-            <span className="text-green-300 hover:text-green-500">
-              <FontAwesomeIcon icon={faChevronCircleUp} />
-              {stop.properties.upvotes}
-            </span>
-            <span className="text-red-300 hover:text-red-500">
-              <FontAwesomeIcon icon={faChevronCircleDown} />
-              {stop.properties.downvotes}
-            </span>
-          </div>
-          <div className="flex-1 pl-1 mr-16">
-            <div className="font-medium">{stop.properties.name}</div>
-            <div className="text-sm text-gray-600">
-              {' '}
-              {stop.properties.label}
-              {stop.properties.description}
+      <>
+        <li className="z-10 flex flex-row mb-2 border-gray-400">
+          <div className="flex items-center flex-1 p-2 transition duration-500 ease-in-out transform bg-gray-200 rounded-md cursor-pointer select-none hover:-translate-y-1 hover:shadow-lg">
+            <div className="flex flex-col items-center justify-center w-10 h-10 mr-4 bg-gray-300 rounded-md">
+              <span className="text-green-300 hover:text-green-500">
+                <FontAwesomeIcon icon={faChevronCircleUp} />
+                {stop.properties.upvotes}
+              </span>
+              <span className="text-red-300 hover:text-red-500">
+                <FontAwesomeIcon icon={faChevronCircleDown} />
+                {stop.properties.downvotes}
+              </span>
+            </div>
+            <div className="flex-1 pl-1 mr-16">
+              <div className="font-medium">{stop.properties.name}</div>
+              <div className="text-sm text-gray-600">
+                {' '}
+                {stop.properties.label}
+                {stop.properties.description}
+              </div>
+            </div>
+            <div className="text-xs text-gray-400 hover:text-gray-600">
+              <FontAwesomeIcon
+                icon={faEdit}
+                onClick={(e): void => {
+                  setEditStop(`${stop._id}`);
+                }}
+              />
             </div>
           </div>
-          <div className="text-xs text-gray-400 hover:text-gray-600">
-            <FontAwesomeIcon icon={faEdit} />
-          </div>
-        </div>
-      </li>
+        </li>
+        {editStop === stop._id ? (
+          <li className="z-0 flex flex-row mb-2 -mt-3 border-teal-600">
+            <div className="flex items-center flex-1 p-2 transition duration-500 ease-in-out transform bg-gray-100 rounded-md cursor-pointer select-none">
+              <div className="flex flex-col items-center justify-center w-10 h-10 mr-4 bg-gray-100 rounded-md"></div>
+              <div className="flex-1 pl-1 mr-16">
+                {/* <Formik
+                  initialValues={{
+                    name: stop.properties.name,
+                    description: stop.properties.description,
+                  }}
+                  onSubmit={() => {}}
+                /> */}
+              </div>
+              <div className="text-xs text-teal-600 ">
+                <FontAwesomeIcon
+                  icon={faSave}
+                  onClick={(e): void => {
+                    setEditStop('');
+                  }}
+                />
+              </div>
+            </div>
+          </li>
+        ) : null}
+      </>
     );
   };
 
@@ -64,7 +98,7 @@ export default function TripEdit(): JSX.Element {
       <div className="container flex items-center justify-center w-full mx-auto">
         <ul className="flex flex-col w-full p-4">
           {trip?.details.features.map((stop: Stop, index) => (
-            <TimelineItem key={index} stop={stop} />
+            <StopListItem key={index} stop={stop} />
           ))}
         </ul>
       </div>
--- client/src/types/Stop.ts
+++ client/src/types/Stop.ts
@@ -2,7 +2,7 @@ import { Point } from 'geojson';
 
 export type StopCollection = GeoJSON.FeatureCollection<Point, StopProperties>;
 
-export type Stop = GeoJSON.Feature<Point, StopProperties>;
+export type Stop = { _id?: string } & GeoJSON.Feature<Point, StopProperties>;
 
 type StopProperties = {
   name: string;
