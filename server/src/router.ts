7ea36dd96a04313c1aa927086937664fe73f48f6
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-08-20 10:03:40 +0200

    feat(router): add routes for managing stops

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import * as tripController from './controllers/trip.controller';
 import * as photoController from './controllers/photo.controller';
+import * as stopController from './controllers/stop.controller';
 
 const router = express.Router();
 
@@ -9,5 +10,8 @@ router.get('/trips/:id', tripController.getOneTrip);
 router.post('/trips', tripController.createTrip);
 router.delete('/trips/:id', tripController.deleteTrip);
 router.get('/photos/:queryText', photoController.getPhoto);
+router.get('/stops/:id', stopController.getOneStop);
+router.delete('/trips/:tripId/stops/:stopId', stopController.deleteStop);
+router.post('/trips/:tripId/stops/:stopId', stopController.addStopToTrip);
 
 export default router;
