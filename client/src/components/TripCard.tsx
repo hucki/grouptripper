a88a3817da1f25b92711b7af38031e70d28ed413
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-08-20 20:36:05 +0200

    feature(tripcard): reactivate Pictures

--- client/src/components/TripCard.tsx
+++ client/src/components/TripCard.tsx
@@ -1,27 +1,22 @@
 import React from 'react';
 import dayjs from 'dayjs';
-
-// import { usePhoto } from '../hooks/usePhoto';
-// import { Stop } from './../types/Stop';
+import { usePhoto } from '../hooks/usePhoto';
 import { Trip } from './../types/Trip';
 
 type TripCardProps = {
   trip: Trip;
 };
 
-// export default function TripCard({ trip }: TripCardProps): JSX.Element {
-// const { trip } = props;
-
-// const photo = usePhoto({
-//   queryText: trip.country,
-//   dimensions: { width: 100, height: 100 },
-// });
-
 export default function TripCard({ trip }: TripCardProps): JSX.Element {
+  const photo = usePhoto({
+    queryText: trip.country,
+    dimensions: { width: 100, height: 100 },
+  });
+
   return (
     <div className="flex flex-col items-center justify-center max-w-sm mx-auto my-8">
       <div
-        // style={photo ? { backgroundImage: `url(${photo.imgUrl})` } : {}}
+        style={photo ? { backgroundImage: `url(${photo.imgUrl})` } : {}}
         className="w-full h-48 bg-teal-300 bg-center bg-cover rounded-lg shadow-md"
       ></div>
       <div className="w-full ml-2 mr-2 -mt-16 overflow-hidden bg-white rounded-lg shadow-lg">
