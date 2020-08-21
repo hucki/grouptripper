a9b4d2ae7932ac7c89c8503d32aeed197c3b0fda
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-08-21 13:13:42 +0200

    feature(TripCard): use usePhoto hook to get pics

--- client/src/components/TripCard.tsx
+++ client/src/components/TripCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import dayjs from 'dayjs';
-// import { usePhoto } from '../hooks/usePhoto';
+import { usePhoto } from '../hooks/usePhoto';
 import { Trip } from './../types/Trip';
 
 type TripCardProps = {
@@ -12,35 +12,12 @@ export default function TripCard({
   trip,
   listView,
 }: TripCardProps): JSX.Element {
-  // const photo = usePhoto({
-  //   queryText: trip.country,
-  //   dimensions: { width: 100, height: 100 },
-  // });
+  const photo = usePhoto({
+    queryText: trip.stopsCollection.features[0].properties.name,
+    dimensions: { width: 300, height: 300 },
+  });
 
   return (
-    // <div className="flex flex-col items-center justify-center max-w-sm mx-auto my-8">
-    //   <div
-    //     style={{
-    //       backgroundImage: `url(https://source.unsplash.com/featured/200x200/?${trip?.country})`,
-    //     }}
-    //     className="w-full h-48 bg-teal-300 bg-center bg-cover rounded-lg shadow-md"
-    //   ></div>
-    //   <div className="w-full ml-2 mr-2 -mt-16 overflow-hidden bg-white rounded-lg shadow-lg">
-    //     <div className="py-2 font-bold tracking-wide text-center text-gray-800 uppercase">
-    //       {trip.name}
-    //     </div>
-    //     <div className="flex items-center justify-between px-3 py-2 bg-gray-400">
-    //       <h1 className="font-bold text-gray-800 ">
-    //         {dayjs(trip?.startDate).format('DD.MM.YYYY')} -
-    //         {dayjs(trip?.endDate).format('DD.MM.YYYY')}
-    //       </h1>
-    //       {/* <button className="px-2 py-1 text-xs font-semibold text-white uppercase bg-gray-800 rounded hover:bg-gray-700">
-    //         Add to cart
-    //       </button> */}
-    //     </div>
-    //   </div>
-    // </div>
-
     <div className="flex items-center flex-1 p-1 transition duration-500 ease-in-out transform bg-gray-100 rounded-md cursor-pointer select-none hover:-translate-y-1 hover:shadow-lg">
       <div className="flex flex-col">
         <div className="flex flex-col justify-between leading-normal bg-white rounded-b lg:rounded-b-none lg:rounded-r">
@@ -56,11 +33,13 @@ export default function TripCard({
                 className="flex flex-wrap flex-shrink-0 m-4"
                 style={listView ? {} : { height: '100px', width: '100px' }}
               >
-                <img
-                  src={`https://source.unsplash.com/featured/500x500/?${trip?.country}`}
-                  alt="..."
-                  className="h-auto max-w-full align-middle border-none shadow-lg"
-                />
+                {photo ? (
+                  <img
+                    src={photo.imgUrl}
+                    alt={photo.altDescription}
+                    className="h-auto max-w-full align-middle border-none shadow-lg"
+                  />
+                ) : null}
               </div>
               <div className="flex flex-col p-2">
                 <h3 className="mb-2 text-2xl font-bold text-teal-900">
