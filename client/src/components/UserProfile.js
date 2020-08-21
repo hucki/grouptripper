218622ffc4193ce7172d0fc4617057aad65594cf
Mohammed <mohmed.ak.17@gmail.com>
2020-08-21 17:01:00 +0200

    minor refactor

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -7,18 +7,18 @@ const UserProfile = () => {
   const { logout, user } = useAuth0();
   return (
     <div className="px-4 py-2 mb-4 mr-1 ">
-      <div class="bg-white shadow p-4 rounded">
-        <div class="text-center mt-4">
-          <p class="text-gray-600 font-bold">
-            {user.name ? user.name : 'Mohammed'}
+      <div className="bg-white shadow p-4 rounded">
+        <div className="text-center mt-4">
+          <p className="text-gray-600 font-bold">
+            {user.name ? user.name : ''}
           </p>
-          <p class="text-sm font-hairline text-gray-600 mt-1">
-            {user.email ? user.email : 'mohmed.ak.17@gmail.com'}
+          <p className="text-sm font-hairline text-gray-600 mt-1">
+            {user.email ? user.email : ''}
           </p>
         </div>
-        <div class="flex justify-center mt-4">
+        <div className="flex justify-center mt-4">
           <img
-            class="shadow sm:w-12 sm:h-12 w-20 h-20 rounded-full object-contain h-48 w-full"
+            className="shadow sm:w-12 sm:h-12 w-20 h-20 rounded-full object-contain h-48 w-full"
             src={
               user.picture
                 ? user.picture
@@ -27,7 +27,7 @@ const UserProfile = () => {
             alt="user"
           />
         </div>
-        <div class="flex justify-center mt-4">
+        <div className="flex justify-center mt-4">
           <button
             className="flex text-center justify-center shadow border-teal-500 border-2 rounded-full px-4 py-2 text-xl text-teal-500 uppercase hover:bg-red-500 hover:text-white"
             style={{ transition: 'all .15s ease' }}
