3f1f789084f0edad94c1216f2b5861d7fac78d72
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-08-24 18:46:46 +0200

    feature(DB): provide manual fallback to local DB

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -2,6 +2,10 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
+const DB_LOCAL = process.env.DB_LOCAL;
+const DB_LOCAL_URI = process.env.DB_LOCAL_URI;
+const DB_LOCAL_PORT = process.env.DB_LOCAL_PORT;
+
 const DB_NAME = process.env.DB_NAME;
 const DB_URI_PROTOCOL = process.env.DB_URI_PROTOCOL;
 const DB_URI_HOST = process.env.DB_URI_HOST;
@@ -10,20 +14,33 @@ const DB_USER = process.env.DB_USER;
 const DB_PASS = process.env.DB_PASS;
 const DB_PASS_ENC = encodeURIComponent(DB_PASS ? DB_PASS : '');
 
-mongoose.connect(
-  `${DB_URI_PROTOCOL}://${DB_USER}:${DB_PASS_ENC}@${DB_URI_HOST}/${DB_NAME}?${DB_URI_PARAMS}`,
-  {
+if (DB_LOCAL === 'true') {
+  mongoose.connect(`${DB_LOCAL_URI}:${DB_LOCAL_PORT}/${DB_NAME}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false,
-  }
-);
+  });
+} else {
+  mongoose.connect(
+    `${DB_URI_PROTOCOL}://${DB_USER}:${DB_PASS_ENC}@${DB_URI_HOST}/${DB_NAME}?${DB_URI_PARAMS}`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  );
+}
 
 const connection = mongoose.connection;
 
 connection.once('open', () => {
-  console.log('successfully connected mongodb');
+  console.log(
+    `successfully connected to ${
+      DB_LOCAL === 'true' ? 'LOCAL ' : 'REMOTE '
+    }mongodb`
+  );
 });
 
 export default mongoose;
