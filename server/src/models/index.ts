54496e26f67fe5fb22086ecf4ab5a0f91d263e5c
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-08-24 18:20:10 +0200

    feature(DB): connect to external DB

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -2,16 +2,23 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const DB_HOST = process.env.DB_HOST;
-const DB_PORT = process.env.DB_PORT;
 const DB_NAME = process.env.DB_NAME;
+const DB_URI_PROTOCOL = process.env.DB_URI_PROTOCOL;
+const DB_URI_HOST = process.env.DB_URI_HOST;
+const DB_URI_PARAMS = process.env.DB_URI_PARAMS;
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const DB_PASS_ENC = encodeURIComponent(DB_PASS ? DB_PASS : '');
 
-mongoose.connect(`${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  `${DB_URI_PROTOCOL}://${DB_USER}:${DB_PASS_ENC}@${DB_URI_HOST}/${DB_NAME}?${DB_URI_PARAMS}`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 const connection = mongoose.connection;
 
