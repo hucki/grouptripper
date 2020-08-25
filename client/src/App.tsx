006cb040f4e42b731f7259e1f9c209dce9bc7459
Alex Marshall <alex.k.marshall83@gmail.com>
2020-08-25 15:14:58 +0100

    style(client): Add navigation to Create Trip page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,27 +5,27 @@ import CreateTrip from './components/CreateTrip';
 import TripView from './components/TripView';
 import TripEdit from './components/TripEdit';
 import UserProfile from './components/UserProfile';
-import { withAuthenticationRequired } from '@auth0/auth0-react';
 import Auth0ProviderWithHistory from './components/Auth0ProviderWithHistory';
 import LandingPageNew from './components/LandingPageNew';
 import PageLayout from './components/PageLayout';
+import ProtectedRoute from './components/ProtectedRoute';
 
-interface Prprops {
-  componenent: React.FC;
-  path: string;
-}
-//eslint-disable-next-line
-const ProtectedRoute = ({ componenent, ...props }: Prprops) => (
-  //eslint-disable-next-line
-  <Route component={withAuthenticationRequired(componenent)} {...props} />
-);
-//eslint-disable-next-line
 function App(): JSX.Element {
   return (
     <Router>
       <Auth0ProviderWithHistory>
         <Switch>
-          <ProtectedRoute path="/create-trip" componenent={CreateTrip} />
+          <Route exact={true} path="/">
+            <LandingPageNew Navigation={Navigation} />
+          </Route>
+          <ProtectedRoute
+            path="/create-trip"
+            component={(): JSX.Element => (
+              <PageLayout Navigation={Navigation}>
+                <CreateTrip />
+              </PageLayout>
+            )}
+          />
           <Route exact={true} path="/trips/:id">
             <PageLayout Navigation={Navigation}>
               <TripView />
@@ -35,9 +35,6 @@ function App(): JSX.Element {
             <PageLayout Navigation={Navigation}>
               <TripEdit />
             </PageLayout>
-          </Route>
-          <Route exact={true} path="/">
-            <LandingPageNew Navigation={Navigation} />
           </Route>
           <Route path="/user-profile">
             <PageLayout Navigation={Navigation}>
