5e840b7559bd1bcf4bd12c27fabb3eed956d5479
Alex Marshall <alex.k.marshall83@gmail.com>
2020-08-26 13:46:48 +0100

    style(comment): Style the input box

--- client/src/components/InputComment.js
+++ client/src/components/InputComment.js
@@ -1,36 +1,32 @@
 import React, { useState } from 'react';
 
-export default function InputComment({ onSubmit, user }) {//eslint-disable-line
+export default function InputComment({ onSubmit, user }) {
+  //eslint-disable-line
   const [comment, setComment] = useState(null);
 
-  const handleChange = (event) => {//eslint-disable-line
+  const handleChange = (event) => {
+    //eslint-disable-line
     setComment(event.target.value);
   };
 
-  const handleSubmit = (event) => {//eslint-disable-line
+  const handleSubmit = (event) => {
+    //eslint-disable-line
     event.preventDefault();
     onSubmit({ comment });
     setComment('');
   };
 
   return (
-    <div className="flex items-center mb-2 mt-1">
-      <img
-        className="w-12 h-12 rounded-full mr-2"
-        src={user.picture}
-        alt="user"
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        required
+        name="new comment"
+        value={comment || ''}
+        onChange={handleChange}
+        placeholder="Add a new comment"
+        className="min-w-full p-2 font-light tracking-wide border border-gray-500 rounded text-grey-dark "
       />
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          required
-          name="new comment"
-          value={comment || ''}
-          onChange={handleChange}
-          placeholder="Add a new comment"
-          className="text-grey-dark tracking-wide font-light min-w-full px-2 py-2"
-        />
-      </form>
-    </div>
+    </form>
   );
 }
