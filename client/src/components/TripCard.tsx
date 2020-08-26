a5d845f3aad339e30a8f8b3aee8e732525363e57
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-08-26 10:58:35 +0200

    fix(tripCard): image full object cover keeping aspect ratio

--- client/src/components/TripCard.tsx
+++ client/src/components/TripCard.tsx
@@ -24,8 +24,13 @@ export default function TripCard({
 
   const invitePending = trip?.invitedEmails?.length ? (
     <>
-      <FontAwesomeIcon icon={faUserClock} className="mt-1 ml-2 text-primary" />
-      <span className="ml-1 text-primary">{trip?.invitedEmails?.length}</span>
+      <FontAwesomeIcon
+        icon={faUserClock}
+        className="mt-1 ml-2 text-orange-600"
+      />
+      <span className="ml-1 text-orange-600">
+        {trip?.invitedEmails?.length}
+      </span>
     </>
   ) : (
     <></>
@@ -44,57 +49,42 @@ export default function TripCard({
   );
 
   return (
-    <div className="flex items-center flex-1 p-1 transition duration-500 ease-in-out transform rounded-lg shadow cursor-pointer select-none hover:-translate-y-1 hover:shadow-lg">
-      <div className="flex flex-col">
-        <div className="flex flex-col justify-between leading-normal rounded-b lg:rounded-b-none lg:rounded-r">
-          <div className="">
-            <div
-              className={
-                listView
-                  ? 'flex flex-col justify-center'
-                  : 'flex flex-row justify-center'
-              }
-            >
-              <div
-                className="relative flex flex-wrap flex-shrink-0 m-4"
-                style={listView ? {} : { height: '100px', width: '100px' }}
-              >
-                {photo ? (
-                  <img
-                    src={photo.imgUrl}
-                    alt={photo.altDescription}
-                    className="h-auto max-w-full align-middle border-none shadow-lg"
-                  />
-                ) : null}
-              </div>
-              <div className="flex flex-col p-2">
-                <h3 className="mb-2 text-2xl font-bold text-yellow-900">
-                  {trip.name}
-                </h3>
-                <div className="flex flex-col">
-                  <div className="text-sm">
-                    <span className="font-bold leading-none text-yellow-900">
-                      {dayjs(trip?.startDate).format('DD.MM.YYYY')} -{' '}
-                      {dayjs(trip?.endDate).format('DD.MM.YYYY')}
-                    </span>
-                    {participants}
-                    {invitePending}
-                  </div>
-                </div>
-                <p className="mt-4">
-                  {trip?.stopsCollection.features.map((stop, index: number) => (
-                    <span
-                      key={stop.properties.name}
-                      className="p-2 m-2 text-xs leading-none rounded-full text-secondary bg-primary"
-                    >
-                      {stop.properties.name}
-                    </span>
-                  ))}
-                </p>
-              </div>
-            </div>
+    <div className="overflow-hidden transition duration-500 ease-in-out transform bg-white rounded-lg shadow cursor-pointer select-none hover:-translate-y-1 hover:shadow-lg">
+      <div className="relative h-48 pb-2/3">
+        {photo ? (
+          <img
+            src={photo.imgUrl}
+            alt={photo.altDescription}
+            className="absolute object-cover w-full h-full"
+          />
+        ) : null}
+      </div>
+      <div className="p-2">
+        <h4 className="mt-1 text-xl font-semibold leading-tight uppercase truncate text-secondary">
+          {' '}
+          {trip.name}
+        </h4>
+        <div className="flex items-baseline">
+          <div className="text-xs font-semibold tracking-wide uppercase">
+            <span className="font-bold leading-none text-secondary">
+              {dayjs(trip?.startDate).format('DD.MM.YYYY')} -{' '}
+              {dayjs(trip?.endDate).format('DD.MM.YYYY')}
+            </span>
+            {participants}
+            {invitePending}
           </div>
         </div>
+
+        <p className="mt-2">
+          {trip?.stopsCollection.features.map((stop, index: number) => (
+            <span
+              key={stop.properties.name}
+              className="inline-block px-2 m-1 text-xs font-semibold tracking-wide bg-yellow-300 rounded-full text-secondary"
+            >
+              {stop.properties.name}
+            </span>
+          ))}
+        </p>
       </div>
     </div>
   );
