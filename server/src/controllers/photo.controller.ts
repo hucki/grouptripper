e6121e39eb59dccd678aabae17188cd742955805
Alex Marshall <alex.k.marshall83@gmail.com>
2020-08-26 08:55:42 +0100

    feat(photos): Create usePhotos hook

--- server/src/controllers/photo.controller.ts
+++ server/src/controllers/photo.controller.ts
@@ -3,16 +3,17 @@ import { Request, Response, NextFunction } from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 
-export async function getPhoto(
+export async function getSinglePhoto(
   req: Request,
   res: Response,
   next: NextFunction
 ): Promise<void> {
+  console.log('got to controller');
   const apikey = process.env.UNSPLASH_API_ACCESS_KEY;
-  const { queryText } = req.params;
+  const { query } = req.query;
   try {
     const unsplashPhoto = await fetch(
-      `https://api.unsplash.com/photos/random?featured=true&orientation=landscape&query=${queryText}`,
+      `https://api.unsplash.com/photos/random?featured=true&orientation=landscape&query=${query}`,
       { headers: { Authorization: `Client-ID ${apikey}` } }
     ).then((result) => {
       if (!result.ok) {
@@ -28,13 +29,8 @@ export async function getPhoto(
       return result.json();
     });
 
-    const {
-      id,
-      urls: { raw: imgUrl },
-      alt_description: altDescription,
-    } = unsplashPhoto;
-
-    const photoObject = { id, imgUrl, altDescription };
+    const photoObject = mapPhoto(unsplashPhoto);
+    console.log(photoObject);
 
     res.status(200).json(photoObject);
   } catch (e) {
@@ -42,3 +38,52 @@ export async function getPhoto(
     next(e);
   }
 }
+
+export async function getPhotos(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> {
+  console.log('got to controller');
+  const apikey = process.env.UNSPLASH_API_ACCESS_KEY;
+  const { query, count } = req.query;
+  try {
+    const unsplahsPhotos = await fetch(
+      `https://api.unsplash.com/photos/random?featured=true&orientation=landscape&query=${query}&count=${count}`,
+      { headers: { Authorization: `Client-ID ${apikey}` } }
+    ).then((result) => {
+      if (!result.ok) {
+        return [
+          {
+            id: '001',
+            urls: {
+              raw:
+                'https://images.unsplash.com/photo-1543169108-32ac15a21e05?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+            },
+            alt_description: '...',
+          },
+        ];
+      }
+      return result.json();
+    });
+
+    res.status(200).json(unsplahsPhotos.map(mapPhoto));
+  } catch (e) {
+    console.log(e);
+    next(e);
+  }
+}
+
+type UnsplashPhoto = {
+  id: string;
+  urls: {
+    raw: string;
+  };
+  alt_description: string;
+};
+
+const mapPhoto = ({
+  id,
+  urls: { raw: imgUrl },
+  alt_description: altDescription,
+}: UnsplashPhoto) => ({ id, imgUrl, altDescription });
